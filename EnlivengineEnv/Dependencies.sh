be719502fd096ce2f6b0e23b865ca0f9dfc6fe03
Charles Mailly <charles.mailly@free.fr>
2020-12-22 23:41:38 +0100

    Working on scripts & CI

---
+++ EnlivengineEnv/Dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Platform detection
+platform='unknown'
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	platform='linux'
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	platform='bsd'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	platform='mac'
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	platform='windows'
+elif [[ "$OSTYPE" == "msys" ]]; then
+	platform='windows'
+elif [[ "$OSTYPE" == "win32" ]]; then
+	platform='windows'
+else
+	echo "OSTYPE == ${OSTYPE} not reconized, assuming it's windows"
+	platform='windows'
+fi
+
+if [[ "$platform" != "windows" ]]; then
+	sudo apt-get install -y cmake gcc-9 g++-9 libgl1-mesa-dev
+fi
