0b58c898ab4fd5ab3c945d53510119dd766d4d63
Charles Mailly <charles.mailly@free.fr>
2020-12-23 03:36:19 +0100

    Update GenerateProject.sh

--- EnlivengineEnv/GenerateProject.sh
+++ EnlivengineEnv/GenerateProject.sh
@@ -45,5 +45,13 @@ fi
 compilerFlags="$temp1 $temp2"
 echo "CompilerFlags : $compilerFlags"
 
-cmake $compilerFlags ..
+generatorFlag="" # Default is good most of the time basing on the compilerFlags
+if [[ "$platform" == "windows" ]]; then
+	if [[ "$1" == "gcc" ]]; then
+		generatorFlag='-G "MinGW Makefiles"'
+	fi
+fi
+echo "ProjectGenerator : $generatorFlag"
+
+cmake $compilerFlags ${generatorFlag} ..
 
