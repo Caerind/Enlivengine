4f983910b345caff46ef07d6600162392597c6db
Charles Mailly <charles.mailly@free.fr>
2021-08-04 02:06:30 +0200

    Revert

--- EnlivengineEnv/InstallDependencies.sh
+++ EnlivengineEnv/InstallDependencies.sh
@@ -20,11 +20,5 @@ else
 fi
 
 if [[ "$platform" == "linux" ]]; then
-	sudo apt-get install -y cmake gcc-9 g++-9 build-essential git make cmake autoconf automake \
-libtool pkg-config libasound2-dev libpulse-dev libaudio-dev libjack-dev \
-libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxi-dev \
-libxinerama-dev libxxf86vm-dev libxss-dev libgl1-mesa-dev libdbus-1-dev \
-libudev-dev libgles2-mesa-dev libegl1-mesa-dev libibus-1.0-dev \
-fcitx-libs-dev libsndio-dev libwayland-dev \
-libxkbcommon-dev libdrm-dev libgbm-dev
+	sudo apt-get install -y cmake gcc-9 g++-9 libgl1-mesa-dev
 fi
