45131a821db1dd5415c380d6a996b84d8b439f7b
Caerind <maillyc76@gmail.com>
2021-08-11 00:03:40 +0200

    Prepare for submodules

--- UberScript.sh
+++ UberScript.sh
@@ -18,6 +18,8 @@ Help()
 
 InstallDependencies()
 {
+	git submodule update --init --recursive
+
 	if [[ "$platform" == "linux" ]]; then
 		sudo apt-get install -y cmake gcc-9 g++-9 build-essential mercurial make autoconf automake libtool libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxi-dev libxinerama-dev libxxf86vm-dev libxss-dev libgl1-mesa-dev
 	fi
